e15112ab791392d937a6fc04fdf6e6ea3d1bc29f
Patrick Ching <burning.codex@gmail.com>
2021-10-27 22:21:55 -0600

    removing deconstructor

--- src/path_analyze.c
+++ src/path_analyze.c
@@ -16,32 +16,16 @@ int path_count(const char *path, const char sep)
 	return to_ret;
 }
 
-int path_deconstruct(const char *path, struct dir_s **list, 
-	int count, const char sep)
-{
-	int in_count = 0;
-	*list = (struct dir_s *) malloc(sizeof(struct dir_s) 
-		* count);
-	if (!*list) {
-		return 0;
-	}
-
-	
-
-	return (count == in_count);
-}
-
 struct path_s *path_analyze(const char *path, const char sep)
 {
 	struct path_s *to_ret = 0x0;
 	int count = path_count(path, sep) + 1;
 
-	to_ret = (struct path_s *) malloc(sizeof(struct path_s));
+	to_ret = (struct path_s *) malloc(sizeof(struct path_s) * 
+		count);
 	if (!to_ret) {
 		goto exit;
 	}
-	path_deconstruct(path, &to_ret->root_list, count, sep);
-
 exit:
 	return to_ret;
 }
